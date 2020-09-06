96f74597509307d5b26c07a82498afd1487888df
Andrey Khomutov <homutovan@live.com>
2020-09-06 17:44:50 +0700

    add class and tests

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# js env starter
+[![Build status](https://ci.appveyor.com/api/projects/status/s4lf5dlknqhd6s04/branch/master?svg=true)](https://ci.appveyor.com/project/homutovan/ajs-1-10-2-symbols-generators/branch/master)
 
-[![Build status](https://ci.appveyor.com/api/projects/status/pgje30jtk2oevnyy/branch/master?svg=true)](https://ci.appveyor.com/project/homutovan/starter/branch/master)
+# Домашнее задание к лекции «Символы, итераторы, генераторы»
+
+## Символы и генераторы
 
 ### Installation
 
---
+++ src/js/team.js
@@ -0,0 +1,24 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) throw new Error('This character is already in the team!');
+    this.members.add(character);
+  }
+
+  addAll(characterList) {
+    this.members = new Set([...this.members, ...characterList]);
+  }
+
+  toArray() {
+    return [...this.members];
+  }
+
+  * [Symbol.iterator]() {
+    for (const person of this.members) {
+      yield person;
+    }
+  }
+}
